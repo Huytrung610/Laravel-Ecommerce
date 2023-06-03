136dbda59e8b362cf2c715d831c05281086317ab
NguyenAnhDung <dungktt26868@gmail.com>
2023-06-04 02:11:13 +0700

    WA-100

---
+++ resources/views/frontend/popup/order-detail-popup.blade.php
@@ -0,0 +1,79 @@
+  <div class="modal fade bd-example-modal-xl" tabindex="-1" role="dialog" aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-xl" role="document">
+      <div class="modal-content">
+        <div class="modal-header" style="padding:1px;">
+          <h3 class="modal-title" id="exampleModalCenterTitle">Order Information</h3>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+  
+        <div class="modal-body">
+              <div class="container">
+                <div class="row">
+                  <div class="col">
+                    <p class="title"><strong>Order ID:</strong> 
+                        <span>12345</span>
+                    </p>
+                    <p class="title"><strong>Delivery date:</strong> 
+                        <span>2023-06-01</span>
+                    </p>
+                    <p class="title"><strong>Status:</strong> 
+                        <span class="status-new">New</span>
+                    </p>
+                    <hr>
+                    <p class="title"><strong>Contact Info</strong> </p>
+                    <p class="subtitle"><strong>Name:</strong> 
+                        <span>Nguyễn Anh Dũng</span>
+                    </p>
+                    <p class="subtitle"><strong>Email:</strong>
+                         <span>Dung123@gmailcom</span>
+                        </p>
+                    <p class="subtitle"><strong>Phone:</strong> 
+                        <span>0999999999</span>
+                    </p>
+                  </div>
+                  <div class="col">
+                    <p class="title"><strong>Order Item:</strong> </p>
+                    <div id="order_product">
+                      <p class="order-item">
+                        <span>iphone 14 pro</span>
+                    </p>
+                      <p class="order-item">
+                        <span>iphone 14 promax 256gb gold</span>
+                    </p>
+                      <p class="order-item">
+                        <span>iphone 11 pro black</span>
+                    </p>
+                      <p class="order-item">
+                        <span>iphone 13 promax 128gb white</span>
+                    </p>
+                    </div>
+                  </div>
+                </div>
+      
+                <hr>
+      
+                <div class="row">
+                  <div class="col">
+                    <p class="title"><strong>Payment Method:</strong> 
+                        <span>COD</span>
+                    </p>
+                  </div>
+                  <div class="col">
+                    <p class="title"><strong>Shipping Address:</strong>
+                        <span class="shipping-address">12345 đại cồ việt, PHƯỜNG bách khoa, THÀNH PHỐ HÀ NỘI</span>
+                    </p>
+                    
+                  </div>
+                </div>
+              </div>
+        </div>
+  
+        <div class="modal-footer" style="border:0px;">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  
